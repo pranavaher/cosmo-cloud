7ea681aa7383e6effb0e1626e4ccbd1f59749738
Pranav Aher <doingdifferent29@gmail.com>
2024-04-07 13:47:01 +0530

    Add class for StudentResponse in model.

--- app/routes/student_routes.py
+++ app/routes/student_routes.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, HTTPException, Query
-from app.controllers.student_controller import create_student, get_all_students
-from app.models.student import StudentCreate, StudentList
+from app.controllers.student_controller import create_student, get_all_students, get_student_by_id
+from app.models.student import StudentCreate, StudentList, StudentResponse
+
+from bson import ObjectId
 
 router = APIRouter()
 
@@ -12,3 +14,12 @@ async def create_student_route(student_data: StudentCreate):
 @router.get("/students", response_model=list[StudentList])
 async def get_all_students_route(country: str = Query(None), age: int = Query(None)):
   return await get_all_students(country, age)
+
+@router.get("/students/{student_id}", response_model=StudentResponse)
+async def get_student_by_id_route(student_id: str):
+  student_id = ObjectId(student_id)
+  student = await get_student_by_id(student_id)
+  if student:
+    return student
+  else:
+    raise HTTPException(status_code=404, detail="Student not found")
