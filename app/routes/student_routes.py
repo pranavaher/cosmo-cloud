b1e22dd29c2114e3681eb8b7ea866fed6576dae4
Pranav Aher <doingdifferent29@gmail.com>
2024-04-07 08:51:06 +0530

    Add route for getting all students.

--- app/routes/student_routes.py
+++ app/routes/student_routes.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, HTTPException
-from app.controllers.student_controller import create_student
-from app.models.student import StudentCreate
+from fastapi import APIRouter, HTTPException, Query
+from app.controllers.student_controller import create_student, get_all_students
+from app.models.student import StudentCreate, StudentList
 
 router = APIRouter()
 
@@ -8,3 +8,7 @@ router = APIRouter()
 async def create_student_route(student_data: StudentCreate):
   student_id = await create_student(student_data)
   return {"id": student_id}
+
+@router.get("/students", response_model=list[StudentList])
+async def get_all_students_route(country: str = Query(None), age: int = Query(None)):
+  return await get_all_students(country, age)
