bc68697f23f3dc5682f0cbe8918f81b0753bafb6
Pranav Aher <doingdifferent29@gmail.com>
2024-04-07 14:42:48 +0530

    Add controller action to update student.

--- app/controllers/student_controller.py
+++ app/controllers/student_controller.py
@@ -1,6 +1,6 @@
 from fastapi import HTTPException
 
-from app.models.student import StudentCreate, StudentList, StudentResponse
+from app.models.student import StudentCreate, StudentList, StudentResponse, StudentUpdate, Address
 from app.db import collection
 
 from typing import Optional
@@ -36,3 +36,15 @@ async def get_student_by_id(student_id: ObjectId):
       return StudentResponse(**student)
     else:
       raise HTTPException(status_code=404, detail="Student not found")
+
+async def update_student(id: str, student_update: StudentUpdate):
+  student_id = ObjectId(id)
+  updated_values = {k: v for k, v in student_update.dict().items() if v is not None}
+  if not updated_values:
+    raise HTTPException(status_code=400, detail="No fields provided for update")
+  if 'address' in updated_values:
+    # Convert Address object to a dictionary before saving to MongoDB
+    updated_values['address'] = student_update.address.dict()
+  result = await collection.update_one({"_id": student_id}, {"$set": updated_values})
+  if result.matched_count == 0:
+    raise HTTPException(status_code=404, detail="Student not found")
