e93c39bf56f5794e5e5bed05cd3d82649d9f949a
Pranav Aher <doingdifferent29@gmail.com>
2024-04-07 14:41:51 +0530

    Add class StudentUpdate update user by id.

--- app/models/student.py
+++ app/models/student.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 class Address(BaseModel):
-  city: str
-  country: str
+  city: Optional[str]
+  country: Optional[str]
 
 class StudentCreate(BaseModel):
   name: str
@@ -18,3 +18,8 @@ class StudentResponse(BaseModel):
   name: str
   age: int
   address: Address
+
+class StudentUpdate(BaseModel):
+  name: Optional[str]
+  age: Optional[int]
+  address: Address = Field(default_factory=dict)
